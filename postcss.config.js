bcea47553bfd49b5167b6a3013cb6f272ad9d1e3
sali6000 <s.ferlazzo@protonmail.com>
2026-03-01 18:09:59 +0100

    Refresh site

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require('postcss-import'),      // pour gérer les @import
-    require('postcss-nesting'),     // CSS imbriqué
-    require('autoprefixer'),        // ajout des prefixes
-    require('cssnano')({ preset: 'default' })
-  ].filter(Boolean)
-};
