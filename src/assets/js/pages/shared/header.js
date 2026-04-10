746435aa9ff4f654de3172ebf44be7394dbcb46e
sali6000 <steven.ferlazzo1@gmail.com>
2026-04-10 21:27:12 +0200

    Stop tracking src/assets (now ignored)

--- src/assets/js/pages/shared/header.js
+++
@@ -1,8 +0,0 @@
-// pages/home.js
-import initMenuHamburger from '@js/components/hamburger';
-
-// Fonction d'initialisation pour la page d'accueil
-// Elle est appelée dans app.js en fonction de la page courante
-export default function () {
-    initMenuHamburger();    // ← exécute le code
-}
