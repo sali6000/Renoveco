944ecd48fc54cc5ac79be3ae24539e6843e9e5fd
sali6000 <steven.ferlazzo1@gmail.com>
2026-05-11 12:52:59 +0200

    Changement de prefixe reveal

--- src/assets/js/components/reveal.js
+++ src/assets/js/components/reveal.js
@@ -1,28 +1,28 @@
 export default function initReveal() {
 
-    const elements = document.querySelectorAll(".reveal, .reveal-infiny");
+    const elements = document.querySelectorAll(".a-reveal, .a-reveal--infiny");
 
     const observer = new IntersectionObserver((entries, obs) => {
         entries.forEach(entry => {
-            const isInfinite = entry.target.classList.contains("reveal-infiny");
+            const isInfinite = entry.target.classList.contains("a-reveal--infiny");
 
             if (entry.isIntersecting) {
 
                 // Gestion du delay pour les éléments "normaux" uniquement
                 if (!isInfinite) {
-                    const siblings = [...entry.target.parentElement.querySelectorAll(".reveal")];
+                    const siblings = [...entry.target.parentElement.querySelectorAll(".a-reveal")];
                     const index = siblings.indexOf(entry.target);
                     entry.target.style.transitionDelay = `${index * 0.05}s`;
                 }
 
-                entry.target.classList.add("visible");
+                entry.target.classList.add("is-visible");
 
                 // Si ce n'est pas un infini, on arrête l'observation
                 if (!isInfinite) obs.unobserve(entry.target);
 
             } else if (isInfinite) {
                 // Pour les éléments infinies, on retire la classe visible quand on sort
-                entry.target.classList.remove("visible");
+                entry.target.classList.remove("is-visible");
             }
         });
     }, { threshold: 0.2 });
