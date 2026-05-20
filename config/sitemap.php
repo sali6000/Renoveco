2f6570472e4cc26f2874e0a174d020b397768bd3
sali6000 <steven.ferlazzo1@gmail.com>
2026-05-20 16:05:43 +0200

    maj sitemap

--- config/sitemap.php
+++ config/sitemap.php
@@ -3,42 +3,42 @@
 return [
 
     [
-        'loc' => 'https://renoveconstruct.be',
+        'loc' => '',
         'lastmod' => '2026-03-28',
         'priority' => '1.0',
         'changefreq' => 'weekly',
     ],
 
     [
-        'loc' => 'https://renoveconstruct.be/services/chassis-fenetres',
+        'loc' => '/services/chassis-fenetres',
         'lastmod' => '2026-05-19',
         'priority' => '0.8',
         'changefreq' => 'yearly',
     ],
 
     [
-        'loc' => 'https://renoveconstruct.be/services/panneaux-photovoltaiques',
+        'loc' => '/services/panneaux-photovoltaiques',
         'lastmod' => '2026-05-19',
         'priority' => '0.8',
         'changefreq' => 'yearly',
     ],
 
     [
-        'loc' => 'https://renoveconstruct.be/about',
+        'loc' => '/about',
         'lastmod' => '2026-03-28',
         'priority' => '0.8',
         'changefreq' => 'monthly',
     ],
 
     [
-        'loc' => 'https://renoveconstruct.be/contact',
+        'loc' => '/contact',
         'lastmod' => '2026-03-28',
         'priority' => '0.8',
         'changefreq' => 'yearly',
     ],
 
     [
-        'loc' => 'https://renoveconstruct.be/cgu',
+        'loc' => '/cgu',
         'lastmod' => '2026-03-28',
         'priority' => '0.3',
         'changefreq' => 'yearly',
