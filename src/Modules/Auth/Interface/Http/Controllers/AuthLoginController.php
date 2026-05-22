1cf04840b354a72009b08f4b5bbea04d08df307c
sali6000 <steven.ferlazzo1@gmail.com>
2026-05-22 10:02:46 +0200

    Correction home logo

--- src/Modules/Auth/Interface/Http/Controllers/AuthLoginController.php
+++ src/Modules/Auth/Interface/Http/Controllers/AuthLoginController.php
@@ -73,9 +73,6 @@ class AuthLoginController extends BaseController
     public function logout()
     {
         $this->session->destroy();
-        header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
-        header('Pragma: no-cache');
-        header('Expires: Sat, 01 Jan 2000 00:00:00 GMT');
         $this->redirect('/');
     }
 }
